e68ee90c920d0ce00ad381e9f459aead8728d44e
Aoife Flavin <G00439331@atu.ie>
2025-03-27 19:33:14 +0000

    Lab p1 attempt 2

---
+++ mywork/wikipedia.py
@@ -0,0 +1,28 @@
+# This program saves a wikipedia page as a pdf
+#Author: Aoife Flavin
+
+import requests
+import urllib.parse
+from config import config as cfg
+
+targetUrl = "https://en.wikipedia.org"
+
+apiKey = cfg["htmltopdfkey"]
+#api = "XXXXXXXX"
+
+apiurl = 'https://api.html2pdf.app/v1/generate'
+
+params = {'url': targetUrl,'apiKey': apiKey}
+parsedparams = urllib.parse.urlencode(params)
+requestUrl = apiurl +"?" + parsedparams
+
+response = requests.get(requestUrl)
+print (response.status_code)
+
+result =response.content
+with open("document.pdf", "wb") as handler:
+    handler.write(result)
+
+
+
+
