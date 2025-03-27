66efcf525228920cdc247ad84f6953e31e4910a7
Aoife Flavin <G00439331@atu.ie>
2025-03-27 19:19:05 +0000

    Deleting all of lab 4 to fix my repository

---
+++ assignments/assignment04-github.py
@@ -0,0 +1,3 @@
+#This program will read a file from a repository and replace all the instances of the text "Andrew" with "Aoife" 
+# The program will then commit those changes and push the file back to the repository
+#Author: Aoife Favin
