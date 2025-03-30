ebc8c46008493b1b7c0ff00fd19b4f8f8e4959ae
Aoife Flavin <G00439331@atu.ie>
2025-03-30 20:36:05 +0100

    finished lab 3 and assignment

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -1,3 +1,31 @@
 #This program will read a file from a repository and replace all the instances of the text "Andrew" with "Aoife" 
 # The program will then commit those changes and push the file back to the repository
-#Author: Aoife Favin
+#Author: Aoife Flavin
+
+from github import Github
+from config import config as cfg
+import requests
+
+#load api key from config file
+apiKey = cfg["githubAssignmentApiKey"]
+g = Github(apiKey)
+
+#get the repository
+repo = g.get_repo("aoifeflav/Assignment-4-repo")
+
+#get file info
+fileInfo = repo.get_contents("story.txt")
+urlOfFile = fileInfo.download_url
+
+#download contents
+response = requests.get(urlOfFile)
+contentsOfFile = response.text
+#print(contentsOfFile)
+
+# replace names
+newContents = contentsOfFile.replace("Andrew", "Aoife")
+newContents = newContents.replace(" he ", " she ").replace(" his ", " her ")
+
+# commit and push changes
+commitMessage = "Replaced 'Andrew' with 'Aoife' and changes the pronouns"
+gitHubResponse = repo.update_file(fileInfo.path, commitMessage, newContents, fileInfo.sha)
--- mywork/lab05.03-githubbymodule.py
+++ mywork/lab05.03-githubbymodule.py
@@ -3,4 +3,28 @@
 
 from github import Github
 from config import config as cfg
+import requests
+
+apiKey = cfg["githubapikey"]
+g = Github(apiKey)
+
+repo = g.get_repo("aoifeflav/aprivateone")
+print(repo.clone_url)
+
+fileInfo = repo.get_contents("test.txt")
+urlOfFile = fileInfo.download_url
+
+#print(urlOfFile)
+
+response = requests.get(urlOfFile)
+contentOfFile = response.text
+#print (contentOfFile)
+
+newContents = contentOfFile + " more stuff \n"
+print (newContents)
+
+
+gitHubResponse=repo.update_file(fileInfo.path,"updated by prog",
+newContents,fileInfo.sha)
+#print (gitHubResponse)
 
